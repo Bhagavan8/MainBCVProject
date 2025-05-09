2a698008b987a22511391f78b2d3f8931be1708c
Bhagavan8 <bhagavancv@gmail.com>
2025-05-09 11:52:12 +0530

    new ui design

--- js/contact.js
+++ js/contact.js
@@ -37,12 +37,12 @@ async function handleContactSubmit(e) {
             timestamp: serverTimestamp()
         });
 
-        // Show success message
-        alert('Thank you for your message. We will get back to you soon!');
+        // Show success message using toast
+        showToast('Thank you for your message. We will get back to you soon!');
         e.target.reset();
     } catch (error) {
         console.error('Error submitting contact form:', error);
-        alert('There was an error sending your message. Please try again.');
+        showToast('There was an error sending your message. Please try again.', false);
     }
 }
  
